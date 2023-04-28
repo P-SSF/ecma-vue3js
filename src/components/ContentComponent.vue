1aa9ab25157217947bd83d0324fb0d01e77bf40a
hojae-lee <chosloo2835@gmail.com>
2023-04-28 16:44:21 +0900

    add : code editor 추가 작업 (왼쪽 코드, 오른쪽 뷰어)

--- src/components/ContentComponent.vue
+++ src/components/ContentComponent.vue
@@ -1,16 +1,32 @@
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, onMounted } from 'vue'
+import Editor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css' // Editor's Style
 
 const props = defineProps({
   title: String,
   content: String
 })
+
+let editor
+
+onMounted(() => {
+  editor = new Editor({
+    el: document.querySelector('#editor'),
+    height: '500px',
+    initialEditType: 'markdown',
+    previewStyle: 'vertical'
+  })
+  editor.getMarkdown()
+})
 </script>
+
 <template>
   <div class="content">
     <h2 class="content-subhead">{{ props.title }}</h2>
     <p>
       {{ props.content }}
     </p>
+    <div id="editor"></div>
   </div>
 </template>
